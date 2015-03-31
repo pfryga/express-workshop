b548e4935a23b194ede2aec0da5bc54bd155193b
Piotr Fryga <piotr.fryga@allegro.pl>
2015-03-31 12:46:04 +0200

    io fixes

--- app/chat.js
+++ app/chat.js
@@ -1,10 +1,19 @@
 module.exports = function(io) {
   io.on('connection', function(socket) {
+    var username;
 
-    console.log('Hello socket!');
+    socket.on('new user connected', function (user) {
+        username = user;
+        socket.broadcast.emit('new user connected', user);
+    });
 
     socket.on('chat message', function(msg) {
+      socket.emit('chat message', msg);
       socket.broadcast.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('user disconnected', username);
     });
 
   });
